9cb0af673cfa2ea33e8d72ce3657c640117a63a4
eon <bernardomp.dev@gmail.com>
2023-08-18 19:24:09 -0300

    feat: displayFinalResut Method

--- src/main/java/com/trybe/java/regraprogressao/ExamManager.java
+++ src/main/java/com/trybe/java/regraprogressao/ExamManager.java
@@ -5,40 +5,78 @@ import java.util.Map;
 import java.util.Scanner;
 
 /**
- * ExamRecords Class.
+ * ExamManager Class.
  */
 public class ExamManager {
   private final Scanner scanner = new Scanner(System.in);
   private int numberOfExams;
-  private final Map<String, Integer> examsData = new HashMap<>();
+  private final Map<String, Exam> examsMap = new HashMap<>();
 
   ExamManager() {
-    examsQuantity();
+    numberOfExamsPrompt();
     gatherExamDetails();
+    displayFinalResult();
   }
 
-  private void examsQuantity() {
+  private void numberOfExamsPrompt() {
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     numberOfExams = Integer.parseInt(scanner.nextLine());
   }
-
 
   private String examNamePrompt(int i) {
     System.out.println("Digite o nome da atividade " + (i + 1) + ":");
     return scanner.nextLine();
   }
 
-  private int examtWeightPrompt(int i) {
+  private int examWeightPrompt(int i) {
     System.out.println("Digite o peso da atividade " + (i + 1) + ":");
     return Integer.parseInt(scanner.nextLine());
   }
 
+  private int examScorePrompt(String examName) {
+    System.out.println("Digite a nota obtida para " + examName + ":");
+    return Integer.parseInt(scanner.nextLine());
+  }
+
+  /** gatherExamDetails method. */
   public void gatherExamDetails() {
     for (int i = 0; i < numberOfExams; i++) {
-      String testName = examNamePrompt(i);
-      int testWeight = examtWeightPrompt(i);
+      String examName = examNamePrompt(i);
+      int examWeight = examWeightPrompt(i);
+      int examScore = examScorePrompt(examName);
 
-      examsData.put(testName, testWeight);
+      Exam exam = new Exam(examName, examWeight, examScore);
+      examsMap.put(examName, exam);
+    }
+  }
+
+  private float calculateTotalPercentage() {
+    int totalScore = 0;
+    int totalWeight = 0;
+
+    for (Exam exam : examsMap.values()) {
+      totalScore += exam.score * exam.weight;
+      totalWeight += exam.weight;
+    }
+
+    return (float) totalScore / totalWeight;
+  }
+
+  /** displayFinalResult method. */
+  public void displayFinalResult() {
+    float totalPercentage = calculateTotalPercentage();
+    if (totalPercentage < 85) {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + totalPercentage
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação."
+      );
+    } else {
+      System.out.println(
+          "Parabéns! Você alcançou "
+              + totalPercentage
+              + "%! E Temos o prazer de informar que você obteve aprovação!"
+      );
     }
   }
 }
