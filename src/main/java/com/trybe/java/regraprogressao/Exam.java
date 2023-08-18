900f315b974e2d8beaadfc6d18905bae532c60b8
eon <bernardomp.dev@gmail.com>
2023-08-18 19:23:23 -0300

    feat: Exam Class

---
+++ src/main/java/com/trybe/java/regraprogressao/Exam.java
@@ -0,0 +1,13 @@
+package com.trybe.java.regraprogressao;
+
+class Exam {
+  public String name;
+  public int weight;
+  public int score;
+
+  public Exam(String name, int weight, int score) {
+    this.name = name;
+    this.weight = weight;
+    this.score = score;
+  }
+}
